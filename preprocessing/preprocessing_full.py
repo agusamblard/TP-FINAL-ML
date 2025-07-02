b897a45a337c6e407c6211013de027c020724c17
alejozeta <128553535+kekontas@users.noreply.github.com>
2025-07-02 19:09:00 -0300

    FUNCIONA.. SEPARAMOS DOS HMV PARA Q POPPEE O NO

--- preprocessing/preprocessing_full.py
+++ preprocessing/preprocessing_full.py
@@ -3,11 +3,15 @@ import pandas as pd
 from preprocessing.data_cleanse import limpiar_dataset
 from preprocessing.hmv import hmv_dataset
 from preprocessing.df_to_numeric import df_to_numeric
+from preprocessing.hmv_final_test import hmv_dataset_final
 
 
-def preprocessing_full(df_to_input,train_complete=pd.read_csv('data/processed/train_processed.csv'), precio=False):
+def preprocessing_full(df_to_input,train_complete=pd.read_csv('data/processed/train_processed.csv'), precio=False, final_test=False):
     df_to_input = limpiar_dataset(df_to_input, precio=precio)
-    df_to_input = hmv_dataset(train_complete,df_to_input)
+    if not(final_test):
+        df_to_input = hmv_dataset(train_complete,df_to_input)
+    else:
+        df_to_input = hmv_dataset_final(train_complete, df_to_input)
     df_to_input = df_to_numeric(train_complete, df_to_input)
 
     return df_to_input
